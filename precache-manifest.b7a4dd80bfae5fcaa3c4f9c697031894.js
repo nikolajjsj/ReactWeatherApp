0f828d5309a76ae50d79d8fe39e78e89849aafad
nikolajjsj <nikolaj.app.dev@gmail.com>
2020-09-27 15:33:01 +0200

    Updates

---
+++ precache-manifest.b7a4dd80bfae5fcaa3c4f9c697031894.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf9d8bc080aaa8ce9c37b4bd398b241",
+    "url": "/ReactWeatherApp/index.html"
+  },
+  {
+    "revision": "17c8b1e0c48cecbb3fcd",
+    "url": "/ReactWeatherApp/static/css/main.a4754cc2.chunk.css"
+  },
+  {
+    "revision": "19d43a0e8dd3d83e2d6f",
+    "url": "/ReactWeatherApp/static/js/2.875739e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactWeatherApp/static/js/2.875739e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c8b1e0c48cecbb3fcd",
+    "url": "/ReactWeatherApp/static/js/main.74c9ae5a.chunk.js"
+  },
+  {
+    "revision": "dbbf4d14c37d00dc4b1c",
+    "url": "/ReactWeatherApp/static/js/runtime-main.c72325d3.js"
+  },
+  {
+    "revision": "a26609a9d4db3d135e729f9c309a9612",
+    "url": "/ReactWeatherApp/static/media/city.a26609a9.jpg"
+  }
+]);
